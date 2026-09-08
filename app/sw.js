cae0eb81bf4d6487284b68d2b3f11ab96b7eb32c
Wandeck Nascimento <macdewandeck@MacBook-Pro-de-Wandeck.local>
2026-09-08 02:54:05 -0300

    corrige saida e demonstracao do aplicativo

--- app/sw.js
+++ app/sw.js
@@ -1,4 +1,4 @@
-const CACHE='pedeai-web-v4-email-only-4';
+const CACHE='pedeai-web-v4-email-only-5';
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['/app/','/app/index.html','/app/manifest.webmanifest','../icon.svg'])).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{

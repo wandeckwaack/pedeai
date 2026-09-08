05efcf5fec1dd9c7d9cd8da56d67b6e7bcd6f829
Wandeck Nascimento <macdewandeck@MacBook-Pro-de-Wandeck.local>
2026-09-08 02:15:23 -0300

    forca atualizacao do PWA junto com o apk

--- app/sw.js
+++ app/sw.js
@@ -1,3 +1,3 @@
-const CACHE='pedeai-web-v4-email-only';
+const CACHE='pedeai-web-v4-email-only-2';
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['/app/','/app/index.html','/app/manifest.webmanifest','../icon.svg']))));
 self.addEventListener('fetch',e=>{if(e.request.method==='GET'&&new URL(e.request.url).origin===location.origin)e.respondWith(caches.match(e.request).then(x=>x||fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match('/app/'))))});
